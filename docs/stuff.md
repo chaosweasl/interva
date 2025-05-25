023cf54fab74923d1721c7efe7b491a798871817
weasl <iacobserban17danieloff@gmail.com>
2025-05-25 19:56:34 +0300

    initial commit

---
+++ docs/stuff.md
@@ -0,0 +1,1 @@
+# make sure to use primary, secondary, tertiary etc (daisyui standard colors) for each and every single thing you want to color - I want to be able to implement themes later
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import "../styles/globals.css";
+
+function Homepage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl text-black">Hello world!</h1>
+    </div>
+  );
+}
+
+export default Homepage;
