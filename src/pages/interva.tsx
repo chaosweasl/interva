1cf1c1f4f400f318b3bd8b9cb043e727d20f99af
weasl <iacobserban17danieloff@gmail.com>
2025-05-28 08:17:13 +0300

    play button

--- src/pages/interva.tsx
+++ src/pages/interva.tsx
@@ -1,12 +1,20 @@
+import { Pause, Play } from "lucide-react";
+import { useState } from "react";
+
 export default function interva() {
   const value = 100;
   const state = "FOCUS";
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function handlePlayButton() {
+    setIsPlaying(!isPlaying);
+  }
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-base-100 overflow-hidden">
       {/* For TSX uncomment the commented types below */}
       <div
-        className="radial-progress text-primary -mt-20"
+        className="radial-progress text-primary"
         style={
           {
             "--value": `${value}`,
@@ -42,6 +50,22 @@ export default function interva() {
           </span>
           <span className="text-xl">{state}</span>
         </div>
+      </div>
+      <div className="mt-5">
+        <button
+          className="btn btn-ghost btn-circle btn-outline w-15 h-15"
+          onClick={() => handlePlayButton()}
+        >
+          {isPlaying ? (
+            <Pause className="w-7 h-7" />
+          ) : (
+            <Play className="w-7 h-7" />
+          )}
+        </button>
+      </div>
+      <div className="flex flex-row justify-between w-full">
+        <div className="ml-10">a</div>
+        <div className="mr-10">b</div>
       </div>
     </div>
   );
