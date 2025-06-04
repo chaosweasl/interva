b5ebee204e048198aed81c9213bba2d1a1be1b58
weasl <iacobserban17danieloff@gmail.com>
2025-06-04 09:54:07 +0300

    few changes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,35 +10,44 @@ export default function about() {
   }
 
   return (
-    <div className="flex flex-col justify-between items-center min-h-screen bg-base-100">
-      <div className="flex flex-col items-center w-full gap-10 h-full p-5 pt-16">
-        <h1 className="text-2xl">Ahoy there!!</h1>
-        <p className="-mt-5">
+    <div className="flex flex-col items-center min-h-screen bg-base-100">
+      <div className="flex flex-col items-center w-full max-w-2xl gap-8 h-full p-6 pt-8 pb-10 bg-base-200 rounded-xl shadow-lg mt-10 mb-10">
+        <h1 className="text-3xl font-bold text-primary">Ahoy there!!</h1>
+        <h2 className="font-semibold text-secondary -mt-6">
+          Thank you so much for using my app!
+        </h2>
+        <p className="-mt-3 text-base-content/80">
           I've built this simple pomodoro app with a few extra features to
           ensure that you can customize it the way you want to.
         </p>
-        <p>
+        <p className="text-base-content/80">
           Originally, this project was built as a project to add to my
           portfolio, but I've since decided to keep it public and free of use to
           anyone who wants to use it.
         </p>
-        <p>
-          Your feedback is well-appreciated, so please feel free to give me
-          honest feedback on this form. Criticism is welcome as well, please let
-          me know what you'd love to see in this project, I'd be more than happy
-          to hear your suggestions!
-        </p>
-        <button className="btn btn-ghost" onClick={handleOpenForm}>
-          Leave your feedback here!
-        </button>
-        <p>
+        <p className="text-base-content/80 text-center max-w-xl">
           Projects like these take a lot of time to make, and I don't make
           anything from them. I have a big dream of starting my own startup to
           help students study more efficiently - your contribution would be
           heart-melting to me. ❤️
         </p>
-        <button className="btn btn-ghost" onClick={handleSupport}>
+        <button
+          className="btn btn-accent btn-wide shadow-md"
+          onClick={handleSupport}
+        >
           Support me on Ko-Fi
+        </button>
+        <p className="text-base-content/80">
+          Your feedback is well-appreciated, so please feel free to give me
+          honest feedback on this form. Criticism is welcome as well, please let
+          me know what you'd love to see in this project, I'd be more than happy
+          to hear your suggestions!
+        </p>
+        <button
+          className="btn btn-primary btn-wide shadow-md"
+          onClick={handleOpenForm}
+        >
+          Leave your feedback here!
         </button>
       </div>
     </div>
