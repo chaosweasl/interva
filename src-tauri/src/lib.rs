aa026295cab611a0cf4a3e259bc62228c427a34d
weasl <iacobserban17danieloff@gmail.com>
2025-06-08 14:29:49 +0300

    check updates on open app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use tauri_plugin_autostart::MacosLauncher;
+use tauri_plugin_updater::UpdaterExt;
 
+// Optional command from your earlier code
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -12,10 +13,44 @@ pub fn run() {
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_autostart::init(
             MacosLauncher::LaunchAgent,
-            Some(vec!["--flag1", "--flag2"]), /* arbitrary number of args to pass to your app */
+            Some(vec!["--flag1", "--flag2"]),
         ))
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            let handle = app.handle().clone();
+            tauri::async_runtime::spawn(async move {
+                // Run the update check
+                if let Err(e) = check_for_updates(handle).await {
+                    println!("Updater error: {:?}", e);
+                }
+            });
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+// This async function does the update check & install
+async fn check_for_updates(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
+    if let Some(update) = app.updater()?.check().await? {
+        let mut downloaded = 0;
+
+        update
+            .download_and_install(
+                |chunk_length, content_length| {
+                    downloaded += chunk_length;
+                    println!("Downloaded {downloaded} of {:?}", content_length);
+                },
+                || {
+                    println!("Download complete");
+                },
+            )
+            .await?;
+
+        println!("Update installed. Restarting...");
+        app.restart();
+    }
+
+    Ok(())
 }
